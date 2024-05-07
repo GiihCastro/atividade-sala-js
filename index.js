640eb3f6770ff1b3390be8f54d4c2dd32b979563
Giovanna <160510344+GiihCastro@users.noreply.github.com>
2024-05-07 10:29:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+// Atividade em sala
+
+// Código que armazena
+
+let num1, num2, num3
+
+num1 = parseFloat(prompt("Insira sua nota de História"))
+num2 = parseFloat(prompt("Insira sua nota de Geografia"))
+num3 = parseFloat(prompt("Insira sua nota de Sociologia"))
+
+var media = (num1 + num2 + num3) / 3
+
+// Código que retorna
+
+let num4, num5, num6
+
+num4 = parseFloat(prompt("Insira sua nota de Física"))
+num5 = parseFloat(prompt("Insira sua nota de Química"))
+num6 = parseFloat(prompt("Insira sua nota de Biologia"))
+
+var media2 = (num4 + num5 + num6) / 3
+
+console.log(`Sua média em Exatas é ${media2}`)
